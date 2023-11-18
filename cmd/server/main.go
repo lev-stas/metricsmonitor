876bdd1df1dec0fe0fdaddcf3c29fae91c9c334a
Stanislav Levchenko <staslevchenko@protonmail.com>
2023-11-19 00:57:40 +0300

    Iteration 6, added server logging

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/lev-stas/metricsmonitor.git/internal/configs"
+	"github.com/lev-stas/metricsmonitor.git/internal/logger"
 	"github.com/lev-stas/metricsmonitor.git/internal/memstorage"
 	"github.com/lev-stas/metricsmonitor.git/internal/routers"
 	"log"
@@ -11,9 +12,12 @@ import (
 var storage *memstorage.MemStorage
 
 func main() {
+	if err := logger.LogInit(configs.ServerParams.LogLevel); err != nil {
+		log.Fatalln(err)
+	}
 	configs.GetServerConfigs()
 	storage = memstorage.NewMemStorage()
 	r := routers.RootRouter(storage)
-	log.Fatalln(http.ListenAndServe(configs.ServerParams.Host, r))
+	log.Fatalln(http.ListenAndServe(configs.ServerParams.Host, logger.RequestResponseLogger(r)))
 
 }
---
+++ internal/logger/requestresponselogger.go
@@ -0,0 +1,41 @@
+package logger
+
+import (
+	"go.uber.org/zap"
+	"net/http"
+	"time"
+)
+
+func RequestResponseLogger(h http.Handler) http.Handler {
+	logFunc := func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		uri := r.RequestURI
+		method := r.Method
+
+		responseData := &responseData{
+			status: 0,
+			size:   0,
+		}
+
+		lw := logResponseWriter{
+			ResponseWriter: w,
+			responseData:   responseData,
+		}
+
+		h.ServeHTTP(&lw, r)
+
+		duration := time.Since(start)
+
+		Log.Info("Incoming request",
+			zap.String("URI", uri),
+			zap.String("method", method),
+			zap.Duration("duration", duration),
+		)
+		Log.Info("Sent response",
+			zap.Int("status", responseData.status),
+			zap.Int("response size", responseData.size),
+		)
+
+	}
+	return http.HandlerFunc(logFunc)
+}
