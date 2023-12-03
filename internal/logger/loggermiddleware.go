fa3c7bf276f3e449771f2deaa21fcf7bd1d60fe2
Stanislav Levchenko <staslevchenko@protonmail.com>
2023-12-03 23:19:00 +0300

    iter9 fixed structure and naming of logger

--- internal/logger/loggermiddleware.go
+++ internal/logger/loggermiddleware.go
@@ -9,6 +9,28 @@ import (
 	"time"
 )
 
+type (
+	responseData struct {
+		status int
+		size   int
+	}
+	logResponseWriter struct {
+		http.ResponseWriter
+		responseData *responseData
+	}
+)
+
+func (r *logResponseWriter) Write(b []byte) (int, error) {
+	size, err := r.ResponseWriter.Write(b)
+	r.responseData.size += size
+	return size, err
+}
+
+func (r *logResponseWriter) WriteHeader(statusCode int) {
+	r.ResponseWriter.WriteHeader(statusCode)
+	r.responseData.status = statusCode
+}
+
 func RequestResponseLogger(h http.Handler) http.Handler {
 	logFunc := func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
